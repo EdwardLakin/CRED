28631d2d75cdf3ea457ea4d22b8b8ea0baff12bb
EdwardLakin <edwardlakin35@gmail.com>
2026-06-15 20:58:40 -0600

    Add diagnostic procedure progress tracking

--- src/features/diagnostic-procedures/index.ts
+++ src/features/diagnostic-procedures/index.ts
@@ -1,1 +1,4 @@
 export { uploadAndExtractDiagnosticProcedure, updateDiagnosticStep, updateDiagnosticProcedureStepExtraction, approveDiagnosticProcedureStructure, attachCaptureToDiagnosticStep } from './actions'
+
+export { getDiagnosticProcedureProgress, getDiagnosticStepCompleteness, captureMatchesDiagnosticStep, getDiagnosticEvidenceRole } from './progress'
+export type { DiagnosticProcedureProgress, DiagnosticStepCompleteness } from './progress'
